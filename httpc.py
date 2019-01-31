130ac90c36290ad8cc8abfa9cab47fa45c635e06
Real Nguyen <real.nguyen@hotmail.com>
2019-01-31 00:26:06 -0500

    - Added flag constants - Added regex to find flags - Added regex to find url - Added is_verbose method - Modified get method to take flags array as a parameter instead of individual flags

--- httpc.py
+++ httpc.py
@@ -1,9 +1,19 @@
 import socket
 import json
 import urllib.parse
+import re
 
 TCP_PORT = 80
 BUFFER_SIZE = 4096 # in bytes
+# Taken from http://amdonnelly.blogspot.com/2014/05/regular-expression-command-line.html
+# Will find command line flags and their parameters
+FLAGS_REGEX = r"(?P<flag>-{1,2}\S*)(?:[=:]?|\s+)(?P<params> [^-\s].*?)?(?=\s+[-\/]|$)"
+# Taken from https://stackoverflow.com/questions/6038061/regular-expression-to-find-urls-within-a-string
+URL_REGEX = r"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?"
+FLAG_VERBOSE = '-v'
+FLAG_HEADERS = '-h'
+FLAG_DATA = '-d'
+FLAG_FILE = '-f'
 
 def help():
     description = 'httpc is a curl-like application but supports HTTP protocol only.'
@@ -17,7 +27,7 @@ def help():
     print(commands + '\n')
     print(use)
 
-def get(URL, verbose=False, headers=''):
+def get(URL, flags):
     # Scheme is HTTP by default, and the only one we need for this assignment
     parsed_url = urllib.parse.urlparse(URL)
     host = parsed_url[1]
@@ -37,7 +47,7 @@ def get(URL, verbose=False, headers=''):
         buffer_str += result.decode('ASCII')
         result = s.recv(BUFFER_SIZE)
     
-    if verbose:
+    if is_verbose(flags):
         # Prints both response header and JSON response
         print(buffer_str)
     else:
@@ -45,11 +55,29 @@ def get(URL, verbose=False, headers=''):
         # Only print JSON response
         print(split[1])
 
-def form_header(header_str):
+def get_url(query):
+    url = re.search(URL_REGEX, query)
+    return url.group(0)
+
+def get_flags(query):
+    flags = re.findall(FLAGS_REGEX, query)
+    return flags
+
+def get_headers(header_str):
+    headers = []
     split = header_str.split(':')
     key, value = split[0], split[1]
     # Returns JSON object
     return json.loads('{{"{0}":"{1}"}}'.format(key, value))
 
-get('http://httpbin.org/get?course=networking&assignment=1', verbose=True)
+def is_verbose(flags):
+    for flag, params in flags:
+        if flag == FLAG_VERBOSE:
+            return True
+    return False
+
+test_get = "httpc get 'http://httpbin.org/get?course=networking&assignment=1' -v -h Content-Type:application/json -h User-Agent:httpc"
+url = get_url(test_get)
+flags = get_flags(test_get)
+get(url, flags)
 #get('http://httpbin.org/get?course=networking&assignment=1', headers='User-Agent:Concordia-HTTP/1.0')
